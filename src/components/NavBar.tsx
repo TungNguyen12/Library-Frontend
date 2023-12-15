dce2e744000034e0e7d2ae4113ccdb84186ccbaf
TungNguyen12 <nvtung382@gmail.com>
2023-12-15 15:59:03 +0200

    improve readability: component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,35 @@
 import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
+
+// MUI
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import { ShoppingCart } from '@mui/icons-material'
 import MenuIcon from '@mui/icons-material/Menu'
-import Container from '@mui/material/Container'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import MenuItem from '@mui/material/MenuItem'
+
+// React-router-dom
 import { Link, useNavigate } from 'react-router-dom'
-import ShoppingCart from '@mui/icons-material/ShoppingCart'
-import { Badge } from '@mui/material'
+
+// Redux hooks
 import { useAppSelector } from '../hooks/useAppSelector'
 import { useAppDispatch } from '../hooks/useAppDispatch'
+import getTotalQuantity from '../redux/selector/cart/getTotalQuantity'
+
+// Reducers
 import { logOut } from '../redux/reducers/authReducer'
 import { clearStateLogout } from '../redux/reducers/userReducer'
 import { clearHistoryLogout } from '../redux/reducers/loansReducer'
 import { clearCart } from '../redux/reducers/cartReducer'
-import getTotalQuantity from '../redux/selector/cart/getTotalQuantity'
 
 function ResponsiveAppBar() {
   const navigate = useNavigate()
--- src/components/admin/components/BookPagination.tsx
+++ src/components/admin/components/BookPagination.tsx
@@ -1,21 +1,28 @@
 import React, { ReactElement } from 'react'
 
+// MUI
 import { useTheme } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableFooter from '@mui/material/TableFooter'
-import TablePagination from '@mui/material/TablePagination'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
-import IconButton from '@mui/material/IconButton'
 import FirstPageIcon from '@mui/icons-material/FirstPage'
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import LastPageIcon from '@mui/icons-material/LastPage'
-import { Avatar, TableHead, Typography } from '@mui/material'
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from '@mui/material'
+
+// Type
 import Book from '../../../types/book/Book'
 
 interface TablePaginationActionsProps {
