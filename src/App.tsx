1ea915bbaedc215cbb6d2fa2853a8be6332351a3
TungNguyen12 <nvtung382@gmail.com>
2023-12-16 12:33:33 +0200

    improve readability: app.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,65 @@
+import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Pages
 import Layout from './pages/Layout'
 import ErrorPage from './pages/ErrorPage'
-import Cart from './pages/CartPage'
-import UserProfile from './pages/UserPage'
-import SingleBook from './pages/SingleBookPage'
-
-import { Signin } from './pages/SigninPage'
+import CartPage from './pages/CartPage'
+import SingleBookPage from './pages/SingleBookPage'
+import SigninPage from './pages/SigninPage'
 import AdminDashboard from './pages/AdminDashboard'
 import CheckRole from './components/PrivateUserRoute'
-import SignUp from './pages/SignUpPage'
-
+import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
 import MyLoanPage from './pages/MyLoanPage'
+import UserPage from './pages/UserPage'
 
 const App = () => {
+  // Role-Checked Components
+  const CartWithRoleCheck = () => (
+    <CheckRole role="Borrower">
+      <CartPage />
+    </CheckRole>
+  )
+
+  const UserPageWithRoleCheck = () => (
+    <CheckRole role="Borrower">
+      <UserPage />
+    </CheckRole>
+  )
+
+  const MyLoanPageWithRoleCheck = () => (
+    <CheckRole role="Borrower">
+      <MyLoanPage />
+    </CheckRole>
+  )
+
+  const AdminDashboardWithRoleCheck = () => (
+    <CheckRole role="Admin">
+      <AdminDashboard />
+    </CheckRole>
+  )
+
+  // Router Configuration
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Layout />,
       errorElement: <ErrorPage />,
       children: [
-        {
-          path: '',
-          element: <HomePage />,
-        },
-        {
-          path: 'cart',
-          element: (
-            <CheckRole role="Borrower">
-              <Cart />
-            </CheckRole>
-          ),
-        },
-        {
-          path: 'profile',
-          element: (
-            <CheckRole role="Borrower">
-              <UserProfile />
-            </CheckRole>
-          ),
-        },
-        {
-          path: 'myloans',
-          element: (
-            <CheckRole role="Borrower">
-              <MyLoanPage />
-            </CheckRole>
-          ),
-        },
-        {
-          path: ':bookId',
-          element: <SingleBook />,
-        },
-        {
-          path: 'signup',
-          element: <SignUp />,
-        },
-
-        {
-          path: 'signin',
-          element: <Signin />,
-        },
-        {
-          path: 'admin',
-          element: (
-            <CheckRole role="Admin">
-              <AdminDashboard />,
-            </CheckRole>
-          ),
-        },
+        { path: '', element: <HomePage /> },
+        { path: 'cart', element: <CartWithRoleCheck /> },
+        { path: 'profile', element: <UserPageWithRoleCheck /> },
+        { path: 'myloans', element: <MyLoanPageWithRoleCheck /> },
+        { path: ':bookId', element: <SingleBookPage /> },
+        { path: 'signup', element: <SignUpPage /> },
+        { path: 'signin', element: <SigninPage /> },
+        { path: 'admin', element: <AdminDashboardWithRoleCheck /> },
       ],
     },
   ])
+
+  // Render
   return <RouterProvider router={router} />
 }
 
