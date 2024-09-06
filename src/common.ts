5dcaebf58a86c08c9d947726426e97a5e91d0fce
TungNguyen12 <nvtung382@gmail.com>
2024-09-06 11:04:09 +0300

    fix url tail

--- src/common.ts
+++ src/common.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = `https://expressapi.frozenpotato.homes/api/v1/api-docs/`
+export const BASE_URL = `https://expressapi.frozenpotato.homes/api/v1/`
 export const LANDING = `/Library-Frontend/`
